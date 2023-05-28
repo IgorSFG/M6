dcd20feb284c119b69578116baca1901599ff9e5
IgorSFG <igor.garcia@sou.inteli.edu.br>
2023-05-28 20:21:43 -0300

    Projeto Crack

--- pond3/README.md
+++ pond3/README.md
@@ -1,1 +1,4 @@
 # Projeto Crack
+
+
+[!img](/home/igorunix/M6/pond3/runs/detect/predict/image0.jpg)
--- pond3/crack.py
+++ pond3/crack.py
@@ -1,5 +1,4 @@
 from ultralytics import YOLO
-from PIL import Image
 import cv2
 
 # build a new model from YAML
@@ -9,9 +8,10 @@ model = YOLO('yolov8n.pt')
 model.train(data='data.yaml', epochs=2, imgsz=416)
 model.val()
 
-img1 = Image.open("datasets/test/images/1616.rf.c868709931a671796794fdbb95352c5a.jpg")
-img2 = Image.open("datasets/test/images/1675.rf.e3aa3f8d28d0247ef0284dd46dacc29f.jpg")
-img3 = Image.open("datasets/test/images/1686.rf.809fb1b51c607e5cf787e44ef4ddd7b8.jpg")
-inputs = [img1, img2, img3]
+img1 = cv2.imread("datasets/test/images/1616.rf.c868709931a671796794fdbb95352c5a.jpg")
+img2 = cv2.imread("datasets/test/images/1675.rf.e3aa3f8d28d0247ef0284dd46dacc29f.jpg")
+img3 = cv2.imread("datasets/test/images/1686.rf.809fb1b51c607e5cf787e44ef4ddd7b8.jpg")
+img4 = cv2.imread("datasets/test/images/1706.rf.011d213c21ec78896c36728dcbc156f5.jpg")
+images = [img1, img2, img3, img4]
 
-model.predict(source=inputs, imgsz=416, conf=0.25, show=True, save=True)
+model.predict(source=images, conf=0.25, save=True)
